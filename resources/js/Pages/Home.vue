9b82a634e1bda0db616a5d391d03d1dea6fcdf8e
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-13 19:23:38 -0400

    initial setup with laravel and vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Home" />
+    <div class="w-[1440px] mx-auto">
+        <h1 class="text-center">Hello Cool</h1>
+    </div>
+
+</template>
+
+
