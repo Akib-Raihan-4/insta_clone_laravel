60c947276dc559e315fe08a9932e4e3b07fabc56
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-15 16:47:42 -0400

    ready to push using VScode

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -23,7 +23,7 @@ defineProps({
     <Head title="Home" />
     <div class="w-[1440px] mx-auto">
         <h1 class="text-center">
-            Hello Cool
+            I Am using Vue3 
         </h1>
     </div>
 
