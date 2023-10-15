d89ca76865b6c6ddd767f71326801f0d5a5e5df7
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-15 17:05:22 -0400

    importing the icons from the vue-materials

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,30 +1,11 @@
 <script setup>
-import { Head, Link } from "@inertiajs/vue3";
-
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
 </script>
 
 <template>
     <Head title="Home" />
     <div class="w-[1440px] mx-auto">
-        <h1 class="text-center">
-            I Am using Vue3 
+        <h1 class="text-center ">
+            Hello Cool
         </h1>
     </div>
-
 </template>
