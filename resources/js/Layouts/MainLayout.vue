7a98c45904f9d48f0f781e7b789f049d428eba88
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-15 18:46:27 -0400

    Insta logo are stored in the public folder and initial navbar

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -12,8 +12,18 @@ import AccountOutline from 'vue-material-design-icons/AccountOutline.vue';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
 import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue';
 
+let showCreatePost = ref(false)
 </script>
 
 <template>
-
+    <div id = "MainLayout" class="w-full h-screen">
+        <div v-if="$page.url === '/'" id="TopNavHome" class="fixed z-30 block w-full bg-white h-[61px] border-b border-b-gray-300 ">
+            <div class="flex items-center justify-between h-full">
+                <Link href="/">
+                    <img class="w-[105px] ml-6 cursor-pointer" src="/insta-logo.png" alt="">
+                </Link>
+            </div>
+        </div>
+        <!-- <slot /> -->
+    </div>
 </template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,11 +1,10 @@
 <script setup>
+    import MainLayout from '@/Layouts/MainLayout.vue';
 </script>
 
 <template>
     <Head title="Home" />
-    <div class="w-[1440px] mx-auto">
-        <h1 class="text-center ">
-            Hello Cool
-        </h1>
-    </div>
+    <MainLayout>
+        <!-- <div class="bg-green-500">Home</div> -->
+    </MainLayout>
 </template>
